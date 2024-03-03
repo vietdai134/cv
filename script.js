64f68a0f2a66e9d7c40fc34f340aa06810f1b3e4
vietdai134 <vietnguyentran134@gmail.com>
2024-03-03 18:35:31 +0700

    Update web

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("linkAbout").addEventListener("click", function() {
+    document.getElementById("about").scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById("linkSkills").addEventListener("click", function() {
+    document.getElementById("skills").scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById("linkExp").addEventListener("click", function() {
+    document.getElementById("experience").scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById("linkEdu").addEventListener("click", function() {
+    document.getElementById("education").scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById("linkContact").addEventListener("click", function() {
+    document.getElementById("Contact").scrollIntoView({ behavior: 'smooth' });
+});
